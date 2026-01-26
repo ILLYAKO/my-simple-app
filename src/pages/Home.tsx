1b4de1436e1731e2a4fc499e911a85969980b12c
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2026-01-25 19:30:02 -0500

    changed exchangeCodeForToken()

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import LoginButton from "../elements/LoginButton";
 import { useEffect } from "react";
 import { OAUTH_CONFIG } from "../config";
 import axios from "axios";
@@ -13,65 +12,57 @@ const Home = () => {
         const code = params.get("code");
 
         if (code) {
-            console.log("OAuth code received0:", code);
-
+            console.log("OAuth code received:", code);
             exchangeCodeForToken(code);
         }
     }, []);
 
     const exchangeCodeForToken = async (code: string) => {
         try {
-            console.log("OAuth code received1:", code);
             const response = await axios.post(
-                "https://login.questrade.com/oauth2/token",
-                null,
+                "http://localhost:3000/exchange-token",
+                { code },
                 {
-                    params: {
-                        client_id: OAUTH_CONFIG.clientId,
-                        code: code,
-                        grant_type: "authorization_code",
-                        redirect_uri: OAUTH_CONFIG.redirectUri,
-                    },
                     headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Content-Type": "application/json",
                     },
                 },
             );
 
             console.log("Access token response:", response.data);
 
-            /*
-        response.data contains:
-        - access_token
-        - refresh_token
-        - expires_in
-        - token_type
-        */
-
-            // TODO (next steps):
-            // save tokens to Redux or localStorage
-            // clean URL (?code=)
             window.history.replaceState({}, document.title, "/my-simple-app/");
         } catch (error) {
             console.error("Failed to exchange code for token:", error);
         }
     };
 
+
     const goToQuestradeLogin = () => {
-        window.location.href =
-            `https://login.questrade.com/oauth2/authorize` +
-            `?client_id=${OAUTH_CONFIG.clientId}` +
-            `&response_type=code` +
-            `&redirect_uri=${OAUTH_CONFIG.redirectUri}`;
+        const url = new URL("https://login.questrade.com/oauth2/authorize");
+        url.searchParams.append("client_id", OAUTH_CONFIG.clientId);
+        url.searchParams.append("response_type", "code");
+        url.searchParams.append("redirect_uri", OAUTH_CONFIG.redirectUri);
+
+        window.location.href = url.toString();
     };
 
     return (
         <div>
             <h1>Home Page</h1>
-            <button onClick={goToQuestradeLogin}>Login with Questrade</button>
-            <button onClick={() => navigate("/about")}>Go to About Page</button>
-
-            <Link to="/about" className="btn btn-primary">
+            <button
+                onClick={goToQuestradeLogin}
+                className="btn btn-primary m-2"
+            >
+                Login with Questrade
+            </button>
+            <button
+                onClick={() => navigate("/about")}
+                className="btn btn-secondary m-2"
+            >
+                Go to About Page
+            </button>
+            <Link to="/about" className="btn btn-success m-2">
                 Go to About
             </Link>
         </div>
